664818f1601c8cb0a06dd95839219bb0b095d2a4
Guillaume DUPONT <gdupont@student.42.fr>
2021-09-01 16:16:33 +0200

    clean main function a bit

--- sources/socket.cpp
+++ sources/socket.cpp
@@ -19,49 +19,10 @@ typedef int SOCKET;
 typedef struct sockaddr_in SOCKADDR_IN;
 typedef struct sockaddr SOCKADDR;
 
-
-// int main(void)
-// {
-//     SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-// 	sockaddr_in sin;
-
-// 	sin.sin_addr.s_addr = htonl(INADDR_ANY);
-// 	sin.sin_family = AF_INET;
-// 	sin.sin_port = htons(23);
-	
-// 	bind(sock, reinterpret_cast<sockaddr*>(&sin), sizeof(sin));
-// 	std::cout  << "coucou\n";
-// 	listen(sock, 5);
-// 	std::cout  << "coucou\n";
-
-// 	socklen_t taille = sizeof(sin);
-// 	std::cout  << "coucou\n";
-
-// 	int csock = accept(sock, reinterpret_cast<sockaddr*>(&sin), &taille);
-// 	std::cout  << "coucou\n";
-
-// 	closesocket(csock);
-// 	return EXIT_SUCCESS;
-// }
-
-
-
-
-typedef int SOCKET;
-typedef struct sockaddr_in SOCKADDR_IN;
-typedef struct sockaddr SOCKADDR;
-
 #define PORT 1028
  
 int main(void)
 {
-    #if defined (WIN32)
-        WSADATA WSAData;
-        int erreur = WSAStartup(MAKEWORD(2,2), &WSAData);
-    #else
-        int erreur = 0;
-    #endif
-    
     /* Socket et contexte d'adressage du serveur */
     SOCKADDR_IN sin;
     SOCKET sock;
@@ -76,83 +37,81 @@ int main(void)
     
     std::fstream logfile;
 	logfile.open("logfile", std::ios::out);
-    if(!erreur)
-    {
+ 
         /* Création d'une socket */
-        sock = socket(AF_INET, SOCK_STREAM, 0);
-        
-        /* Si la socket est valide */
-        if(sock != INVALID_SOCKET)
-        {
-            printf("La socket %d est maintenant ouverte en mode TCP/IP\n", sock);
-            
-            /* Configuration */
-            sin.sin_addr.s_addr = htonl(INADDR_ANY);  /* Adresse IP automatique */
-            sin.sin_family = AF_INET;                 /* Protocole familial (IP) */
-            sin.sin_port = htons(PORT);               /* Listage du port */
-            sock_err = bind(sock, (SOCKADDR*)&sin, recsize);
-            
-            /* Si la socket fonctionne */
-            if(sock_err != SOCKET_ERROR)
-            {
-                /* Démarrage du listage (mode server) */
-                sock_err = listen(sock, 5);
-                logfile << "Listage du port " << PORT << std::endl;
-                
-                /* Si la socket fonctionne */
-                if(sock_err != SOCKET_ERROR)
-                {
-                    /* Attente pendant laquelle le client se connecte */
-                    logfile << "Patientez pendant que le client se connecte sur le port " << PORT << std::endl;
-                    csock = accept(sock, (SOCKADDR*)&csin, &crecsize);
-                    logfile << "Un client se connecte avec la socket " << csock << " de " <<  inet_ntoa(csin.sin_addr) << " : " << htons(csin.sin_port);
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	
+	/* Si la socket est valide */
+	if(sock != INVALID_SOCKET)
+	{
+		printf("La socket %d est maintenant ouverte en mode TCP/IP\n", sock);
+		
+		/* Configuration */
+		sin.sin_addr.s_addr = htonl(INADDR_ANY);  /* Adresse IP automatique */
+		sin.sin_family = AF_INET;                 /* Protocole familial (IP) */
+		sin.sin_port = htons(PORT);               /* Listage du port */
+		sock_err = bind(sock, (SOCKADDR*)&sin, recsize);
+		
+		/* Si la socket fonctionne */
+		if(sock_err != SOCKET_ERROR)
+		{
+			/* Démarrage du listage (mode server) */
+			sock_err = listen(sock, 5);
+			logfile << "Listage du port " << PORT << std::endl;
+			
+			/* Si la socket fonctionne */
+			if(sock_err != SOCKET_ERROR)
+			{
+				/* Attente pendant laquelle le client se connecte */
+				logfile << "Patientez pendant que le client se connecte sur le port " << PORT << std::endl;
+				csock = accept(sock, (SOCKADDR*)&csin, &crecsize);
+				logfile << "Un client se connecte avec la socket " << csock << " de " <<  inet_ntoa(csin.sin_addr) << " : " << htons(csin.sin_port);
 
-                    int epfd;
-                    epfd = epoll_create(1024);
-                    
-                    static struct epoll_event ev;
-                    ev.events = EPOLLIN | EPOLLPRI | EPOLLERR | EPOLLHUP | EPOLLOUT;
-                    ev.data.fd = csock;
-                    epoll_ctl(epfd, EPOLL_CTL_ADD, csock, &ev);
-                    struct epoll_event *events;
-                    events = (struct epoll_event *)calloc(64, sizeof(ev)); /*??? MAX EVENTS ??*/
-                    while (1) {
-                        // wait for something to do...
-                        std::cout << "ready to wait \n";
-                        int nfds = epoll_wait(epfd, events, 64, -1); /*timeout?*/
-                        std::cout << "j ai recu un event, nfds = " << nfds << "\n";
-                        // for each ready socket
-                        for(int i = 0; i < nfds; i++) {
-                            int fd = events[i].data.fd;
-                            char buffer[2000];
-                            bzero(buffer, 1999);
-                            int ret = recv(fd, &buffer, sizeof(buffer), 0);
-                            std::cout << ret << std::endl;
-                            buffer[1999] = '\0';
-                            std::cout << buffer << std::endl;
-                            // std::cout << strerror(errno) << std::endl;
-                            send(fd, "HTTP/1.1 400 Bad Request", 24, 0);
-                            // close(fd);
-                        }
+				int epfd;
+				epfd = epoll_create(1024);
+				
+				static struct epoll_event ev;
+				ev.events = EPOLLIN | EPOLLPRI | EPOLLERR | EPOLLHUP | EPOLLOUT;
+				ev.data.fd = csock;
+				epoll_ctl(epfd, EPOLL_CTL_ADD, csock, &ev);
+				struct epoll_event *events;
+				events = (struct epoll_event *)calloc(64, sizeof(ev)); /*??? MAX EVENTS ??*/
+				while (1) {
+					// wait for something to do...
+					std::cout << "ready to wait \n";
+					int nfds = epoll_wait(epfd, events, 64, -1); /*timeout?*/
+					std::cout << "j ai recu un event, nfds = " << nfds << "\n";
+					// for each ready socket
+					for(int i = 0; i < nfds; i++) {
+						int fd = events[i].data.fd;
+						char buffer[2000];
+						bzero(buffer, 1999);
+						int ret = recv(fd, &buffer, sizeof(buffer), 0);
+						std::cout << ret << std::endl;
+						buffer[1999] = '\0';
+						std::cout << buffer << std::endl;
+						// std::cout << strerror(errno) << std::endl;
+						send(fd, "HTTP/1.1 400 Bad Request", 24, 0);
+						// close(fd);
+					}
 
-                    }
-                    closesocket(csock);
-                    close(epfd);
-                }
-                else
-                    perror("listen");
-            }
-            else
-                perror("bind");
-            /* Fermeture de la socket client et de la socket serveur */
-            printf("Fermeture de la socket client\n");
-            printf("Fermeture de la socket serveur\n");
-            closesocket(sock);
-            printf("Fermeture du serveur terminée\n");
-        }
-        else
-            perror("socket");
-        return EXIT_SUCCESS;
-    }
+				}
+				closesocket(csock);
+				close(epfd);
+			}
+			else
+				perror("listen");
+		}
+		else
+			perror("bind");
+		/* Fermeture de la socket client et de la socket serveur */
+		printf("Fermeture de la socket client\n");
+		printf("Fermeture de la socket serveur\n");
+		closesocket(sock);
+		printf("Fermeture du serveur terminée\n");
+	}
+	else
+		perror("socket");
+	return EXIT_SUCCESS;
 }
 
